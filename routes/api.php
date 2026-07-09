b9e4f4bb9c1f89e0441e89bc699295275b4f68d7
El Hadji Malick Seye <eljimalickseye@gmail.com>
2026-07-09 23:56:31 +0000

    Add welcome health-check route to api.php

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,15 @@ use App\Http\Controllers\TicketController;
 use App\Http\Controllers\TrackingController;
 
 // Public routes
+Route::get('/', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'Welcome to TepTep API',
+        'version' => '1.0.0',
+        'service' => 'GIE Transport SaaS'
+    ]);
+});
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
